5f13a28ed604e5bf42bdd8e6b5897772376eb3a9
seerapusairam <seerapu.sai123@gmail.com>
2025-10-07 19:28:11 -0400

    Refactor GitLab CI workflow: add validation for Kong config and update API endpoint in health check

--- jwt.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const token = jwt.sign(
-  { iss: "AUT2wMh0lfZpv0vaXBdMZMDHjE3Vql3b" }, // use JWT credential 'key'
-  "Ovsczn0ZzmOxYouHYfzgWTYM0NPweB5w",        // secret
-  { algorithm: "HS256", expiresIn: "1h" }
-);
-
-console.log(token);
